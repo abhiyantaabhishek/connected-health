d188b5edab78693d2bdb366b1dc6d0052fbcf52b
abhiyantaabhishek <abhiyantaabhishek1@yahoo.in>
2021-04-19 23:16:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,22 @@
 # __maintainer__ = "Ronie Martinez"
 # __email__ = "ronmarti18@gmail.com"
 import json
-import random
 import time
-from datetime import datetime
 import pandas as pd
-from flask import Flask, Response, render_template,make_response
-
+from datetime import datetime
+from flask import Flask, Response, render_template
+import pickle
 app = Flask(__name__)
-random.seed()  # Initialize the random number generator
+#import requests
+
+url = "https://luminous-fire-9722.firebaseio.com/Health.json/"
+
+headers = {
+    'application-id': "2AA28C7B-9C0E-99AB-FFF5-75156EC1FF00",
+    'secret-key': "4EA6B2D9-C23B-A54D-FFC8-2E7B96623E00",
+    'application-type': "REST",
+
+    }
 
 
 @app.route('/')
@@ -23,25 +31,29 @@ def index():
 
 @app.route('/chart-data')
 def chart_data():
-    def generate_random_data():
-        df = pd.read_csv("data.csv")
-        ind = 0		
+    def generate_random_data():       
+        file_in = open("data.pickle",'rb')
+        data = pickle.load(file_in)
+        json_data_server = list(data[range(0,2000,10)])
+        file_in.close()          
+        start = 0
+        end = 200-1
+        #responses = requests.request("GET", url, headers=headers)        
+        #json_data_server = json.loads(responses.text)
         while True:
-
-            if(ind == df.size-1):
-                ind=0
-            else:
-                ind=ind+1
+            if(start >= end):
+                #json_data_server = new_json_data_server
+                start = 0
+            start = start+1
             json_data = json.dumps(
                 #{'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
-                {'time': df['date_time'].iloc[ind], 'value': int(df['hr'].iloc[ind])}) 
-                #{'time': df['date_time'].iloc[ind], 'value':  ind})  				
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': json_data_server[start]})
+                #{'time': df['date_time'].iloc[ind], 'value': int(df['hr'].iloc[ind])}) 
+                #{'time': df['date_time'].iloc[ind], 'value':  ind})                  
             yield f"data:{json_data}\n\n"
             time.sleep(0.1)
 
     return Response(generate_random_data(), mimetype='text/event-stream')
-
-
 
 
 if __name__ == '__main__':
